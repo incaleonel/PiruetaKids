dd02fc7b4530c92589dafe57548ac5a7949a5c61
Leonel Inca <inca.leonel@gmail.com>
2023-02-08 22:08:22 -0300

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
+
+
+function App() {
+  return (
+    <div className="App">
+      <ResponsiveAppBar/>
+    </div>
+  );
+}
+
+export default App;
