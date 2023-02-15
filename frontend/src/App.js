9129d20a4c2dd69fe3ed7bb17434be37bde77e83
Leonel Inca <inca.leonel@gmail.com>
2023-02-15 19:00:41 -0300

    feat: cambio de diseño

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,48 @@
 import ResponsiveAppBar from "./components/ResponsiveAppBar";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { teal } from "@mui/material/colors";
+import Product from "./components/Product";
+import Grid from "@mui/material/Grid";
+import { Box } from "@mui/system";
 
-
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: teal["A100"],
+      contrastText: "#ff5252",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "white",
+    },
+  },
+});
 function App() {
   return (
     <div className="App">
-      <ResponsiveAppBar/>
+      <ThemeProvider theme={theme}>
+        <ResponsiveAppBar color="primary" />
+      </ThemeProvider>
+        <Grid spacing={3} container sx={{ justifyContent:'center', mt:5}}>
+          <Grid item>
+            <Product title="CAMISA DE NIÑO" />
+          </Grid>
+          <Grid item>
+            <Product title="CAMISA DE NIÑO" />
+          </Grid>
+          <Grid item>
+            <Product title="CAMISA DE NIÑO" />
+          </Grid>
+          <Grid item>
+            <Product title="CAMISA DE NIÑO" />
+          </Grid>
+          <Grid item>
+            <Product title="CAMISA DE NIÑO" />
+          </Grid>
+        </Grid>
+      
     </div>
   );
 }
--- frontend/src/components/ResponsiveAppBar.js
+++ frontend/src/components/ResponsiveAppBar.js
@@ -1,20 +1,37 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import ChildCareTwoToneIcon from "@mui/icons-material/ChildCareTwoTone";
+import '../index.css';
 
-const pages = ['Productos', 'Quienes ', 'Contacto'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+const pages = ["Productos", "Quiénes somos", "Contacto"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+const label = [
+  { letter: "P", color: "red" },
+  { letter: "I", color: "blue" },
+  { letter: "R", color: "#8d6e63" },
+  { letter: "U", color: "#78909c" },
+  { letter: "E", color: "green" },
+  { letter: "T", color: "#88c100" },
+  { letter: "A", color: "#fc5956" },
+  { letter: " ", color: "white" },
+  { letter: "K", color: "#039be5" },
+  { letter: "I", color: "#8bc34a" },
+  { letter: "D", color: "#ffb300" },
+  { letter: "S", color: "#673ab7" }
+  
+];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -35,30 +52,45 @@ function ResponsiveAppBar() {
     setAnchorElUser(null);
   };
 
+  
+
   return (
-    <AppBar position="static" color='secondary'>
+    <AppBar position="static" >
       <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 1,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'red',
-              textDecoration: 'none',
-            }}
+        <Toolbar disableGutters
+                >
+          <Box display='flex'
+              alignItems='center'
           >
-            P
-          </Typography>
-          
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <ChildCareTwoToneIcon
+              sx={{ display: { xs: "none", md: "flex" }, mr: 1, fontSize: 50, position:'relative'}} className='icon-child'
+            />
+            <Toolbar disableGutters>
+              {label.map((e) => {
+                return (
+                  <Typography
+                    variant="h6"
+                    noWrap
+                    component="a"
+                    href="/"
+                    sx={{
+                      p: 0.5,
+                      display: { xs: "none", md: "flex" },
+                      fontFamily: "monospace",
+                      fontWeight: 700,
+                      letterSpacing: ".3rem",
+                      color: e.color,
+                      textDecoration: "none",
+                      userSelect: "none"
+                    }}
+                  >
+                    {e.letter}
+                  </Typography>
+                );
+              })}
+            </Toolbar>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -73,18 +105,18 @@ function ResponsiveAppBar() {
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: { xs: "block", md: "none" },
               }}
             >
               {pages.map((page) => (
@@ -94,31 +126,38 @@ function ResponsiveAppBar() {
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+        
+            <Box
+              display='flex'
+              flexGrow='1'>
+              {label.map((e) => {
+                return (
+                  <Typography
+                    variant="h8"
+                    noWrap
+                    component="a"
+                    href="/"
+                    sx={{
+                      p: .4,
+                      display: { xs: "flex", md: "none" },
+                      fontFamily: "monospace",
+                      fontWeight: 600,
+                      color: e.color,
+                      textDecoration: "none",
+                      userSelect: "none"
+                    }}
+                  >
+                    {e.letter}
+                  </Typography>
+                );
+              })}
+            </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ my: 3, color: "black", display: "block" , fontWeight:"500", fontFamily:"sans-serif"}}
               >
                 {page}
               </Button>
@@ -128,21 +167,21 @@ function ResponsiveAppBar() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
-              sx={{ mt: '45px' }}
+              sx={{ mt: "45px" }}
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
