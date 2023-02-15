4258cb75da31c5084c0318a843a7805bd66bb5df
Leonel Inca <inca.leonel@gmail.com>
2023-02-15 09:12:46 -0300

    feat: add card design

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import ModalBody from './ModalBody';
+function Product(props) {
+    
+      return (
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea sx={{ display:'flex', flexDirection:'column', justifyContent:'center'}} >
+            <CardMedia
+              component="img"
+              height="400"
+              image="https://blog.trapitos.com.ar/uploads/2018/07/camisa-de-tela-vertical.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="div" align='center'>
+                {props.title}
+              </Typography>
+              <Typography gutterBottom variant="h4" component="div" align='center'>
+                $2999
+              </Typography>
+            </CardContent>
+            <ModalBody />
+          </CardActionArea>
+        </Card>
+      );
+}
+
+export default Product;
